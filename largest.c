a538c1b8f10298813d15c46c42ca738276a35171
nithiyavsb <31506096+nithiyavsb@users.noreply.github.com>
2018-01-18 11:20:22 +0000

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,12 @@
+# include(stdio.h>
+main()
+{
+int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if(a<b && a<c);
+printf("%d",a is largest number);
+eles
+printf("%d",a is not largest number);
+}
